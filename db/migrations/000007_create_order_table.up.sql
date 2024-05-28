5d48eb72071c050baa15da22147ff5be51737b96
sarandn <sariandini755@gmail.com>
2024-05-28 23:44:32 +0700

    edit tabel order

--- db/migrations/000007_create_order_table.up.sql
+++ db/migrations/000007_create_order_table.up.sql
@@ -1,7 +1,9 @@
 CREATE TABLE Order (
-    order_id SERIAL PRIMARY KEY,
-    user_id INT,
-    order_date TIMESTAMP,
-    total_amount INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    order_id serial PRIMARY KEY,
+    user_id int NOT NULL,
+    order_date timestamp DEFAULT CURRENT_TIMESTAMP,
+    total_amount int,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_users_order FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
