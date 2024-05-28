2e2ae4cdf293df56ed0b6199b9c33ec96cde0130
sarandn <sariandini755@gmail.com>
2024-05-28 21:49:49 +0700

    menambahkan tabel order dan tabel order detail

---
+++ db/migrations/000007_create_order_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "Order" (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT,
+    order_date TIMESTAMP,
+    total_amount INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
---
+++ db/migrations/000008_create_order_detail_table.up.sql
@@ -0,0 +1,10 @@
+Copy code
+CREATE TABLE OrderDetail (
+    order_detail_id SERIAL PRIMARY KEY,
+    order_id INT,
+    ticket_id INT,
+    quantity INT,
+    subtotal INT,
+    FOREIGN KEY (order_id) REFERENCES "Order"(order_id),
+    FOREIGN KEY (ticket_id) REFERENCES Ticket(ticket_id)
+);
