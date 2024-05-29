ad5b2aa1afaf5c7564b5003aca1535e6e7fc0974
Enrico Zada <enricozada00@gmail.com>
2024-05-29 19:38:41 +0700

    fix migration create table

--- db/migrations/000007_create_order_table.up.sql
+++ db/migrations/000007_create_order_table.up.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Order (
+CREATE TABLE orders (
     order_id serial PRIMARY KEY,
-    user_id int NOT NULL,
+    user_id_fk int NOT NULL,
     order_date timestamp DEFAULT CURRENT_TIMESTAMP,
     total_amount int,
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_users_order FOREIGN KEY(user_id) REFERENCES users(user_id)
+    CONSTRAINT fk_users_order FOREIGN KEY(user_id_fk) REFERENCES users(user_id)
 );
